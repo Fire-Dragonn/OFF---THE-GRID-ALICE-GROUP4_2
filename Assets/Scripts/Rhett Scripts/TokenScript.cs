7cad7ef1de6c60cfe4c079febbde936ffb00b1cd
RhettiBOi <163155031+RhettiBOi@users.noreply.github.com>
2024-06-17 21:51:26 +0200

    final

--- Assets/Scripts/Rhett Scripts/TokenScript.cs
+++ Assets/Scripts/Rhett Scripts/TokenScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+//using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 using static UnityEngine.GraphicsBuffer;
 
 public class TokenScript : MonoBehaviour
